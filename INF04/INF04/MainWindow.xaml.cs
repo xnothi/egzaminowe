3828420d24e0bcd6dbfadc1ec6b335b347da1659
sigma <nothingoopl@gmail.com>
2024-04-24 14:55:18 +0200

    UI Czerwiec 2023

---
+++ INF04/INF04/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace INF04
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {      
+            Styczen2024 styczen2024 = new Styczen2024();
+            styczen2024.Show();
+
+            this.Close();
+            
+        }
+    }
+}
---
+++ INF04/INF04/Styczen2024.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace INF04
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Styczen2024.xaml
+    /// </summary>
+    public partial class Styczen2024 : Window
+    {
+        public Styczen2024()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+           MainWindow mainWindow = new MainWindow();
+           mainWindow.Show();
+           this.Close();  
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
